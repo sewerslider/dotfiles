e5303bb3071831203efa80897095c189e2617a3e
sewerlider <hvstuff4@proton.me>
2024-08-01 23:37:52 +0700

    update installer script

--- .config/installer/command.sh
+++ .config/installer/command.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
+# install necessary stuff for this script to work
+sudo pacman -S base base-devel linux linux-firmware
 
 # install pikaur
 rm -rf pikaur
 git clone https://aur.archlinux.org/pikaur.git
 cd pikaur
 makepkg -sic
-pikaur $(pacman -Qqe)
+pikaur -Syu $(pacman -Qqe)
 
-systemctl enable --now --user pipewire pipewire-pulse wireplumber
+grub_install() {
+	grub-install --efi-directory=/efi --target=x86_64-efi --bootloader-id=ARCH
+}
+
+systemd_service() {
+	systemctl enable NetworkManager
+	systemctl enable --user pipewire pipewire-pulse wireplumber
+}
+
+grub_install
+systemd_service
